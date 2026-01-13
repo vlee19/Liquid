a87ba15d4dd885daa0226e4bd34a834703761159
Valerie Lee <leevalerie1990@gmail.com>
2026-01-13 01:23:37 -0500

    fixed arrow size

--- src/App.jsx
+++ src/App.jsx
@@ -282,7 +282,7 @@ export default function App() {
 						}
 						setOpenLinkBox(prev => !prev);}}
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#3c3c3ca1"><path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#3c3c3ca1"><path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
 				</button>
 			)}
             <div className={`input-container ${openLinkBox ? "links-open" : ""}`}>
